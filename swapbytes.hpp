4112bcdb1f52ce0f5e4566645795ea754512826b
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2013-07-31 18:07:47 +0000

    strips

--- swapbytes.hpp
+++ swapbytes.hpp
@@ -1,8 +1,6 @@
 #ifndef SWAPBYTES_HPP
 # define SWAPBYTES_HPP
 
-#include <algorithm>
-
 #include <cassert>
 
 #include <cstdint>
@@ -10,38 +8,58 @@
 #ifdef __GNUC__
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int64_t& u)
+static inline std::int64_t swap_bytes(std::int64_t u)
 {
+  return __builtin_bswap64(u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint64_t& u)
+static inline std::uint64_t swap_bytes(std::uint64_t u)
 {
+  return __builtin_bswap64(u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int32_t& u)
+static inline std::int32_t swap_bytes(std::int32_t u)
 {
+  return __builtin_bswap32(u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint32_t& u)
+static inline std::uint32_t swap_bytes(std::uint32_t u)
 {
+  return __builtin_bswap32(u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int16_t& u)
+static inline std::int16_t swap_bytes(std::int16_t const u)
 {
+  return __builtin_bswap16(u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint16_t& u)
+static inline std::uint16_t swap_bytes(std::uint16_t const u)
 {
+  return __builtin_bswap16(u);
 }
 
 #else
+
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int64_t& u)
+static inline std::int64_t swap_bytes(std::int64_t u)
+{
+  return (u >> 56) |
+    ((u << 40) & 0x00ff000000000000ull) |
+    ((u << 24) & 0x0000ff0000000000ull) |
+    ((u << 8)  & 0x000000ff00000000ull) |
+    ((u >> 8)  & 0x00000000ff000000ull) |
+    ((u >> 24) & 0x0000000000ff0000ull) |
+    ((u >> 40) & 0x000000000000ff00ull) |
+    (u << 56);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+static inline std::uint64_t swap_bytes(std::uint64_t u)
 {
   u = (u >> 56) |
     ((u << 40) & 0x00ff000000000000ull) |
@@ -54,60 +72,33 @@ static inline void swap_bytes(std::int64_t& u)
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint64_t& u)
+static inline std::int32_t swap_bytes(std::int32_t u)
 {
-  u = (u >> 56) |
-    ((u << 40) & 0x00ff000000000000ull) |
-    ((u << 24) & 0x0000ff0000000000ull) |
-    ((u << 8)  & 0x000000ff00000000ull) |
-    ((u >> 8)  & 0x00000000ff000000ull) |
-    ((u >> 24) & 0x0000000000ff0000ull) |
-    ((u >> 40) & 0x000000000000ff00ull) |
-    (u << 56);
-}
-
-//////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int32_t& u)
-{
-  u = (u >> 24) |
+  return (u >> 24) |
     ((u << 8) & 0x00ff0000u) |
     ((u >> 8) & 0x0000ff00u) |
     (u << 24);
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint32_t& u)
+static inline std::uint32_t swap_bytes(std::uint32_t u)
 {
-  u = (u >> 24) |
+  return (u >> 24) |
     ((u << 8) & 0x00ff0000u) |
     ((u >> 8) & 0x0000ff00u) |
     (u << 24);
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::int16_t& u)
+static inline std::uint16_t swap_bytes(std::int16_t u)
 {
-  u = (u >> 8) |
-    (u << 8);
+  return (u >> 8) | (u << 8);
 }
 
 //////////////////////////////////////////////////////////////////////////////
-static inline void swap_bytes(std::uint16_t& u)
+static inline std::uint16_t swap_bytes(std::uint16_t u)
 {
-  u = (u >> 8) |
-    (u << 8);
-}
-
-//////////////////////////////////////////////////////////////////////////////
-template <class T>
-static inline void swap_bytes(T& u)
-{
-  std::uint8_t* v(reinterpret_cast<std::uint8_t*>(&u));
-
-  for (std::uint8_t i(0); i != sizeof(u) >> 1; ++i)
-  {
-    std::swap(v[i], v[sizeof(u) - 1]);
-  }
+  return (u >> 8) | (u << 8);
 }
 
 #endif // __GNUC__
