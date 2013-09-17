c598255342fd8dc3d53180ff1c179a6b5cfe0b68
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2013-09-17 19:33:07 +0000

    some fixes

--- cformat.hpp
+++ cformat.hpp
@@ -26,7 +26,39 @@ struct cformat_error : ::std::runtime_error
 
 typedef cformat_error wcformat_error;
 
-::std::string cformat(char const*, ...);
+//////////////////////////////////////////////////////////////////////////////
+inline ::std::string cformat(char const* format, ...)
+{
+  va_list ap;
+
+  va_start(ap, format);
+
+  ::std::string::size_type const len(::std::vsnprintf(0, 0, format, ap) + 1);
+
+#ifdef _MSC_VER
+  #include "alloca.h"
+  char* const s(static_cast<char*>(_malloca(len)));
+#else
+  char s[len];
+#endif
+
+  va_end(ap);
+
+  va_start(ap, format);
+
+  if (::std::vsprintf(s, format, ap) < 0)
+  {
+    va_end(ap);
+
+    throw cformat_error();
+  }
+  else
+  {
+    va_end(ap);
+
+    return {s, len - 1};
+  }
+}
 
 //////////////////////////////////////////////////////////////////////////////
 template <::std::size_t buffer_size = 128>
