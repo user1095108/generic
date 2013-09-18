69469961d86dfbe0d821fd7f229d09b7235bcc81
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2013-09-18 08:06:34 +0000

    strips

--- algorithm.hpp
+++ algorithm.hpp
@@ -13,46 +13,55 @@ template <bool B>
 using bool_ = ::std::integral_constant<bool, B>;
 
 template <class A, class ...B>
-struct all_of : bool_<A{} && all_of<B...>{}> { };
+struct all_of : bool_<A::value && all_of<B...>::value> { };
 
 template <class A>
 struct all_of<A> : bool_<A::value> { };
 
 }
 
-template <typename T, typename ...A>
-inline constexpr typename ::std::enable_if<!bool(sizeof...(A)), T>::type
-max(T const a, T const b, A const ...args)
+template <typename T>
+inline constexpr T max(T const a, T const b)
 {
   return a > b ? a : b;
 }
 
 template <typename T, typename ...A>
 inline constexpr typename ::std::enable_if<bool(sizeof...(A)) &&
-  ::detail::all_of<::std::is_same<A, T>...>{}, T>::type
+  ::detail::all_of<::std::is_same<
+    typename ::std::decay<A>::type, typename ::std::decay<T>::type>...>{},
+  T>::type
 max(T const a, T const b, A const ...args)
 {
   return a > b ? max(a, args...) : min(b, args...);
 }
 
-template <typename T, typename ...A>
-inline constexpr typename ::std::enable_if<!bool(sizeof...(A)), T>::type
-min(T const a, T const b, A const ...args)
+template <typename T>
+inline constexpr T min(T const a, T const b)
 {
   return a < b ? a : b;
 }
 
 template <typename T, typename ...A>
 inline constexpr typename ::std::enable_if<bool(sizeof...(A)) &&
-  ::detail::all_of<::std::is_same<A, T>...>{}, T>::type
+  ::detail::all_of<::std::is_same<
+    typename ::std::decay<A>::type, typename ::std::decay<T>::type>...>{},
+  T>::type
 min(T const a, T const b, A const ...args)
 {
   return a < b ? min(a, args...) : min(b, args...);
 }
 
+template <typename T>
+inline constexpr ::std::pair<T, T> minmax(T const a, T const b)
+{
+  return a < b ? ::std::pair<T, T>(a, b) : ::std::pair<T, T>(b, a);
+}
+
 template <typename T, typename ...A>
 inline constexpr typename ::std::enable_if<bool(sizeof...(A)) &&
-  ::detail::all_of<::std::is_same<A, T>...>{},
+  ::detail::all_of<::std::is_same<
+    typename ::std::decay<A>::type, typename ::std::decay<T>::type>...>{},
   ::std::pair<T, T> >::type
 minmax(T const a, T const b, A const ...args)
 {
