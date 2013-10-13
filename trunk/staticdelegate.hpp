1738ed0529fb3fd89db727e6aa1d0d27fcde73f6
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2013-10-13 01:15:53 +0000

    strips

--- trunk/staticdelegate.hpp
+++ trunk/staticdelegate.hpp
@@ -190,14 +190,14 @@ public:
   delegate(::std::nullptr_t const) noexcept : delegate() { }
 
   template <class C, typename =
-    typename ::std::enable_if<::std::is_class<C>{}>::type>
+    typename ::std::enable_if< ::std::is_class<C>{}>::type>
   explicit delegate(C const* const o) noexcept :
     object_ptr_(const_cast<C*>(o))
   {
   }
 
   template <class C, typename =
-    typename ::std::enable_if<::std::is_class<C>{}>::type>
+    typename ::std::enable_if< ::std::is_class<C>{}>::type>
   explicit delegate(C const& o) noexcept :
     object_ptr_(const_cast<C*>(&o))
   {
@@ -436,7 +436,7 @@ private:
   struct is_member_pair : std::false_type { };
 
   template <class C>
-  struct is_member_pair<::std::pair<C* const,
+  struct is_member_pair< ::std::pair<C* const,
     R (C::* const)(A...)> > : std::true_type
   {
   };
@@ -445,7 +445,7 @@ private:
   struct is_const_member_pair : std::false_type { };
 
   template <class C>
-  struct is_const_member_pair<::std::pair<C const* const,
+  struct is_const_member_pair< ::std::pair<C const* const,
     R (C::* const)(A...) const> > : std::true_type
   {
   };
