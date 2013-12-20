d4715a27ca7d9f0832385ce3a4dedb8f774a3190
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2013-12-20 07:22:23 +0000

    some fixes

--- trunk/string.hpp
+++ trunk/string.hpp
@@ -3,10 +3,37 @@
 
 #include <cstring>
 
+#include <algorithm>
+
 #include <string>
 
 namespace generic
 {
+
+// join
+//////////////////////////////////////////////////////////////////////////////
+template <typename C>
+inline typename C::value_type join(C const& container,
+  typename C::value_type::value_type const sep)
+{
+  if (container.size())
+  {
+    typename C::value_type r(container.front());
+
+    auto const end(container.cend());
+
+    for (typename C::const_iterator i(container.cbegin() + 1); i != end; ++i)
+    {
+      r += sep + *i;
+    }
+
+    return r;
+  }
+  else
+  {
+    return typename C::value_type();
+  }
+}
 
 // trimming
 //////////////////////////////////////////////////////////////////////////////
