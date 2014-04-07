c1bc23bf581b1c76accab10bcf9b17a2dafd28ac
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-04-07 11:11:55 +0000

    some fixes

---
+++ forwarder.hpp
@@ -0,0 +1,64 @@
+#include <cstdint>
+
+#include <cstddef>
+
+#include <type_traits>
+
+#include <new>
+
+namespace generic
+{
+
+template<typename F>
+class forwarder;
+
+template<typename R, typename ... A>
+class forwarder<R (A...)>
+{
+public:
+	template<typename T>
+	function(T const& functor) :
+		stub_(&handler<T>::invoke)
+  {
+		static_assert(sizeof(T) <= sizeof(store_), "functor too large");
+		static_assert(std::is_trivially_destructible<T>::value,
+				"functor not trivially destructible");
+		static_assert(std::is_trivially_copy_constructible<T>::value,
+				"functor not trivially copy constructible");
+		static_assert(std::is_trivially_copy_assignable<T>::value,
+				"functor not trivially copy assignable");
+		new (&store_) handler<T>(functor);
+	}
+
+	R operator() (A... args)
+  {
+		return stub_(&store_, args...);
+	}
+
+private:
+	template<typename T>
+	struct handler
+  {
+		T functor_;
+		
+		handler(const T &functor) : functor_(functor) { }
+
+		static R invoke(void *ptr, A... args)
+    {
+			return static_cast<handler<T>*>(ptr)->functor_(args...);
+		}
+	};
+
+#if defined(__clang__)
+  using max_align_type = long double;
+#elif defined(__GNUC__)
+  using max_align_type = ::max_align_t;
+#else
+  using max_align_type = ::std::max_align_t;
+#endif
+
+	alignof(max_align_type) uintptr_t store_;
+	R (*stub_)(void*, A...);
+};
+
+}
