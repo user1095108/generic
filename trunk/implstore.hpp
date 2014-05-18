377518a69a4b3db744544aa115fbaa47f8357c62
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-05-18 10:23:28 +0000

    some fixes

--- trunk/implstore.hpp
+++ trunk/implstore.hpp
@@ -17,72 +17,54 @@ template <class U, ::std::size_t N = 64>
 class implstore
 {
 public:
-  using value_type = U;
-
   static constexpr ::std::size_t buffer_size = N;
 
-  implstore()
-  {
-    static_assert(sizeof(U) <= sizeof(store_),
-      "impl too large");
-    static_assert(::std::is_default_constructible<U>{},
-      "impl is not default constructible");
-    static_cast<void*>(new (&store_) U);
-
-    deleter_ = deleter_stub;
-  }
+  using value_type = U;
 
   template <typename ...A, typename =
-    typename ::std::enable_if<::std::is_constructible<U, A...>::type> >
-  explicit implstore(A&& ...args)
+    typename ::std::enable_if<::std::is_constructible<U, A...>{}>::type>
+  implstore(A&& ...args)
   {
     static_assert(sizeof(U) <= sizeof(store_),
       "impl too large");
-    static_cast<void*>(new (&store_) U(::std::forward<A>(args)...));
+    new (static_cast<void*>(&store_)) U(::std::forward<A>(args)...);
 
     deleter_ = deleter_stub;
   }
 
   ~implstore() { if (deleter_) deleter_(*this); }
 
-  template <::std::size_t M>
+  template <::std::size_t M, typename K = U, typename =
+    typename ::std::enable_if<::std::is_copy_constructible<K>{}>::type>
   implstore(implstore<U, M> const& other)
   {
-    static_assert(::std::is_copy_constructible<U>{},
-      "impl is not copy constructible");
-    new (&store_) U(*other);
+    new (static_cast<void*>(&store_)) U(*other);
 
     deleter_ = other.deleter_;
   }
 
-  template <::std::size_t M>
+  template <::std::size_t M, typename K = U, typename =
+    typename ::std::enable_if<::std::is_move_constructible<K>{}>::type>
   implstore(implstore<U, M>&& other)
   {
-    static_assert(::std::is_move_constructible<U>{},
-      "impl is not move constructible");
-
-    new (&store_) U(::std::move(*other));
+    new (static_cast<void*>(&store_)) U(::std::move(*other));
 
     deleter_ = other.deleter_;
   }
 
-  template <::std::size_t M>
+  template <::std::size_t M, typename K = U, typename =
+    typename ::std::enable_if<::std::is_copy_assignable<K>{}>::type>
   implstore& operator=(implstore<U, M> const& other)
   {
-    static_assert(::std::is_copy_assignable<U>{},
-      "impl is not copy assignable");
-
     **this = *other;
 
     return *this;
   }
 
-  template <::std::size_t M>
+  template <::std::size_t M, typename K = U, typename =
+    typename ::std::enable_if<::std::is_move_assignable<K>{}>::type>
   implstore& operator=(implstore<U, M>&& other)
   {
-    static_assert(::std::is_move_assignable<U>{},
-      "impl is not move assignable");
-
     **this = ::std::move(*other);
 
     return *this;
@@ -121,15 +103,11 @@ public:
   explicit operator bool() const noexcept { return deleter_; }
 
 private:
-  static void deleter_stub(implstore& is)
-  {
-    typedef char type_must_be_complete[sizeof(U) ? 1 : -1];
-    (void)sizeof(type_must_be_complete);
-    is->~U();
-  }
+  static void deleter_stub(implstore& is) { is->~U(); }
 
 private:
-  void (*deleter_)(implstore&){};
+  using deleter_type = void(*)(implstore&);
+  deleter_type deleter_;
 
   typename ::std::aligned_storage<N>::type store_;
 };
