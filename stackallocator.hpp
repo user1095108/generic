02ff8d5b7e2a4eb0831d60edba9d17fe0cc01868
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-05-19 07:22:05 +0000

    some fixes

--- stackallocator.hpp
+++ stackallocator.hpp
@@ -6,11 +6,9 @@
 
 #include <cstddef>
 
-#include <functional>
-
-#include <new>
-
 #include <utility>
+
+#include <type_traits>
 
 namespace generic
 {
@@ -67,11 +65,14 @@ public:
     }
   }
 
-  void reset() noexcept { ptr_ = buf_; }
+  void reset() noexcept { ptr_ = reinterpret_cast<char*>(&buf_); }
 
   static constexpr ::std::size_t size() noexcept { return N; }
 
-  ::std::size_t used() const noexcept { return ::std::size_t(ptr_ - buf_); }
+  ::std::size_t used() const noexcept
+  {
+    return ::std::size_t(ptr_ - reinterpret_cast<char*>(&buf_));
+  }
 
 private:
   static constexpr ::std::size_t align(::std::size_t const n) noexcept
@@ -81,23 +82,17 @@ private:
 
   bool pointer_in_buffer(char* const p) noexcept
   {
-    return (buf_ <= p) && (p <= buf_ + N);
+    return (reinterpret_cast<char*>(buf_) <= p) &&
+      (p <= reinterpret_cast<char*>(buf_) + N);
   }
 
 private:
-#if defined(__clang__)
-  using max_align_type = long double;
-#elif defined(__GNUC__)
-  using max_align_type = ::max_align_t;
-#else
-  using max_align_type = ::std::max_align_t;
-#endif
+  char* ptr_{reinterpret_cast<char*>(buf_)};
 
-  static constexpr auto const alignment = alignof(max_align_type);
+  ::std::aligned_storage<N> buf_;
 
-  char* ptr_{buf_};
-
-  alignas(max_align_type) char buf_[N];
+  constexpr static ::std::size_t const alignment{
+    alignof(typename decltype(buf_)::type)};
 };
 
 template <class T, std::size_t N>
