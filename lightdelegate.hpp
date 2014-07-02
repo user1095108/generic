9feb75ffecc7680898783d44bbf6f27407b6a9ff
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-07-02 09:54:14 +0000

    some fixes

--- lightdelegate.hpp
+++ lightdelegate.hpp
@@ -15,34 +15,34 @@ namespace detail
 {
   // stubs
   template <typename FP, FP fp, class R, class ...A>
-  static R function_stub(A&&... args)
+  static constexpr R function_stub(A&&... args)
   {
     return fp(::std::forward<A>(args)...);
   }
 
   template <typename FP, FP fp, class C, class R, class ...A>
-  static R member_function_stub(C* const object, A&&... args)
+  static constexpr R member_function_stub(C* const object, A&&... args)
   {
     return (object->*fp)(::std::forward<A>(args)...);
   }
 
+  // factories
   template <typename FP, FP fp, class R, class ...A>
-  static constexpr auto make_delegate(R (* const)(A...)) ->
+  inline constexpr auto make_delegate(R (* const)(A...)) ->
     decltype(&function_stub<FP, fp, R, A...>)
   {
     return function_stub<FP, fp, R, A...>;
   }
 
-  // factories
   template <typename FP, FP fp, class C, class R, class ...A>
-  static constexpr auto make_delegate(R (C::* const)(A...)) ->
+  inline constexpr auto make_delegate(R (C::* const)(A...)) ->
     decltype(&member_function_stub<FP, fp, C, R, A...>)
   {
     return member_function_stub<FP, fp, C, R, A...>;
   }
 
   template <typename FP, FP fp, class C, class R, class ...A>
-  static constexpr auto make_delegate(R (C::* const)(A...) const) ->
+  inline constexpr auto make_delegate(R (C::* const)(A...) const) ->
     decltype(&member_function_stub<FP, fp, C, R, A...>)
   {
     return member_function_stub<FP, fp, C, R, A...>;
