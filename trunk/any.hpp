c997371b4d099e1017a252e7d3dc42eabf1a6c34
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-11-13 08:30:29 +0000

    some fixes

--- trunk/any.hpp
+++ trunk/any.hpp
@@ -23,7 +23,7 @@ public:
   using typeid_t = void (*)();
 
   template <typename T>
-  static typeid_t type_id() noexcept
+  static constexpr typeid_t type_id() noexcept
   {
     return typeid_t(type_id<T>);
   }
