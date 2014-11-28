261c62a61656d59dfa1b0d3085ae776c6475be91
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2014-11-28 12:33:39 +0000

    some fixes

--- variant.hpp
+++ variant.hpp
@@ -497,23 +497,27 @@ public:
 
   variant& operator=(variant const& rhs)
   {
-    if (!rhs)
+    if (this != &rhs)
     {
-      clear();
-    }
-    else if (rhs.meta_->copier)
-    {
-      rhs.meta_->copier(type_id_ == rhs.type_id_,
-        meta_->deleter, store_, rhs.store_);
+      if (!rhs)
+      {
+        clear();
+      }
+      else if (rhs.meta_->copier)
+      {
+        rhs.meta_->copier(type_id_ == rhs.type_id_,
+          meta_->deleter, store_, rhs.store_);
 
-      meta_ = rhs.meta_;
+        meta_ = rhs.meta_;
 
-      type_id_ = rhs.type_id_;
+        type_id_ = rhs.type_id_;
+      }
+      else
+      {
+        throw ::std::bad_typeid();
+      }
     }
-    else
-    {
-      throw ::std::bad_typeid();
-    }
+    // else do nothing
 
     return *this;
   }
@@ -521,34 +525,38 @@ public:
   template <typename ...U>
   variant& operator=(variant<U...> const& rhs)
   {
-    if (!rhs)
+    if (this != &rhs)
     {
-      clear();
-    }
-    else if (rhs.copier_)
-    {
-      auto const converted_type_id(
-        rhs.template convert_type_id<0, T...>()
-      );
-
-      if (-1 == converted_type_id)
+      if (!rhs)
       {
-        throw ::std::bad_typeid();
+        clear();
+      }
+      else if (rhs.meta_->copier_)
+      {
+        auto const converted_type_id(
+          rhs.template convert_type_id<0, T...>()
+        );
+
+        if (-1 == converted_type_id)
+        {
+          throw ::std::bad_typeid();
+        }
+        else
+        {
+          rhs.meta_->copier(type_id_ == converted_type_id,
+            meta_->deleter, store_, rhs.store_);
+
+          meta_ = rhs.meta_;
+
+          type_id_ = converted_type_id;
+        }
       }
       else
       {
-        rhs.meta_->copier(type_id_ == converted_type_id,
-          meta_->deleter, store_, rhs.store_);
-
-        meta_ = rhs.meta_;
-
-        type_id_ = converted_type_id;
+        throw ::std::bad_typeid();
       }
     }
-    else
-    {
-      throw ::std::bad_typeid();
-    }
+    // else do nothing
 
     return *this;
   }
