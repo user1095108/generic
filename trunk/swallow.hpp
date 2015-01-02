7037d3bf7ddc1a210d7abf1f0ad8258462eb5807
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2015-01-02 20:36:35 +0000

    some fixes

---
+++ trunk/swallow.hpp
@@ -0,0 +1,18 @@
+#ifndef GENERIC_SWALLOW_HPP
+# define GENERIC_SWALLOW_HPP
+# pragma once
+
+namespace generic
+{
+
+struct swallow
+{
+  template <typename ...T>
+  swallow(T&&) noexcept
+  {
+  }
+};
+
+}
+
+#endif // GENERIC_SWALLOW_HPP
