38ccc585f7bab1ef0a19e4a0346c1125064e7850
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2015-02-04 14:16:34 +0000

    some fixes

---
+++ cify.hpp
@@ -0,0 +1,37 @@
+#ifndef GENERIC_CIFY_HPP
+# define GENERIC_CIFY_HPP
+# pragma once
+
+#include <utility>
+
+namespace generic
+{
+
+namespace
+{
+
+template <typename R, typename ...A> using return_type = R (*)(A...);
+
+}
+
+template <typename F, typename R, typename ...A>
+return_type<R, A...> cify(R (*)(A...), F&& f)
+{
+  static F f_(::std::forward<F>(f));
+  
+  new (static_cast<void*>(&f_)) F(::std::forward<F>(f));
+
+  struct S
+  {
+    static R f(A... args)
+    {
+      f_(::std::forward<A>(args)...);
+    }
+  };
+
+  return &S::f;
+}
+
+}
+
+#endif // GENERIC_CIFY_HPP
