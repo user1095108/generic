d3c7a27c08af3cb1864ba2ca84f65d05fe246082
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2015-02-06 08:36:24 +0000

    some fixes

--- trunk/cify.hpp
+++ trunk/cify.hpp
@@ -38,10 +38,44 @@ inline F cify(L&& l, R (*)(A...))
   return &S::f;
 }
 
+template <typename F, int I, typename L, typename R, typename ...A>
+inline F cify(L&& l, R (*)(A...))
+{
+  static thread_local L l_(::std::forward<L>(l));
+  static thread_local bool full;
+
+  if (full)
+  {
+    l_.~L();
+
+    new (static_cast<void*>(&l_)) L(::std::forward<L>(l));
+  }
+  else
+  {
+    full = true;
+  }
+
+  struct S
+  {
+    static R f(A... args) noexcept(noexcept(l_(::std::forward<A>(args)...)))
+    {
+      return l_(::std::forward<A>(args)...);
+    }
+  };
+
+  return &S::f;
+}
+
 }
 
 template <typename F, int I = 0, typename L>
 inline F cify(L&& l)
+{
+  return cify<F, I>(::std::forward<L>(l), F());
+}
+
+template <typename F, int I = 0, typename L>
+inline F thread_local_cify(L&& l)
 {
   return cify<F, I>(::std::forward<L>(l), F());
 }
