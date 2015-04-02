734041195fd187d064995b846fd51be4c6ed9ce1
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2015-04-02 18:51:58 +0000

    some fixes

--- trunk/contains.hpp
+++ trunk/contains.hpp
@@ -7,8 +7,9 @@ namespace generic
 
 // contains
 //////////////////////////////////////////////////////////////////////////////
-template <class Container, class Key>
-inline bool contains(Container const& c, Key const& key) noexcept
+template <class Container>
+inline bool contains(Container const& c,
+  typename Container::key_type const& key) noexcept
 {
   return c.end() != c.find(key);
 }
