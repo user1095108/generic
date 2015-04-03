1808ad422c5ecfb2895c751b97a0e4f3136da953
janezz55@gmail.com <janezz55@gmail.com@c2654bb2-5f8e-e6c3-e4c1-32b6c2fa6cf0>
2015-04-03 15:47:37 +0000

    some fixes

--- trunk/variant.hpp
+++ trunk/variant.hpp
@@ -217,7 +217,8 @@ struct is_copy_assignable<T,
     sizeof((typename T::reference(T::*)())(&T::back)),
     sizeof((typename T::reference(T::*)())(&T::front)),
     sizeof((typename T::value_type*(T::*)())(&T::data)),
-    sizeof((void(T::*)(typename T::const_reference))(&T::push_back))
+    sizeof((void(T::*)(typename T::const_reference))(&T::push_back)),
+    sizeof((void(T::*)(typename T::value_type&&))(&T::push_back))
   )
 > : is_copy_assignable<typename T::value_type>
 {
@@ -245,7 +246,8 @@ struct is_copy_constructible<T,
     sizeof((typename T::reference(T::*)())(&T::back)),
     sizeof((typename T::reference(T::*)())(&T::front)),
     sizeof((typename T::value_type*(T::*)())(&T::data)),
-    sizeof((void(T::*)(typename T::const_reference))(&T::push_back))
+    sizeof((void(T::*)(typename T::const_reference))(&T::push_back)),
+    sizeof((void(T::*)(typename T::value_type&&))(&T::push_back))
   )
 > : is_copy_constructible<typename T::value_type>
 {
