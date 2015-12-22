4553c112cff29e677fc657a253a04a9f39342240
janezz55 <janezz55@gmail.com>
2015-12-22 14:13:17 +0100

    some fixes

---
+++ recurse.hpp
@@ -0,0 +1,17 @@
+#ifndef GENERIC_RECURSE_HPP
+# define GENERIC_RECURSE_HPP
+# pragma once
+
+namespace generic
+{
+
+template<class F>
+auto recurse(F&& f)
+{
+  return [f = std::forward<F>(f)](auto&& ...args)
+  {
+    return f(f, decltype(args)(args)...);
+  };
+}
+
+}
