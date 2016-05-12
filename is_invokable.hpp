6b3508f38d2d09c2407bdb911b52a29d40b98eab
janezz55 <janezz55@gmail.com>
2016-05-12 10:30:02 +0200

    some fixes

--- is_invokable.hpp
+++ is_invokable.hpp
@@ -8,16 +8,18 @@ namespace generic
 namespace
 {
 
-template <typename F>
-auto f(F&& i, int) -> decltype(i(), ::std::integral_constant<bool, true>{});
+template <typename F, typename ...A>
+auto f(int) -> decltype((void)i(::std::declval<A...>()),
+  ::std::integral_constant<bool, true>{}
+);
 
-template <typename F>
-auto f(F&& i, long) -> ::std::integral_constant<bool, false>;
+template <typename F, typename ...A>
+auto f(long) -> ::std::integral_constant<bool, false>;
 
 }
 
-template <typename F>
-struct is_invokable : decltype(f(::std::declval<F>(), 0))
+template <typename F, typename ...A>
+struct is_invokable : decltype(f<F, A...>(0))
 {
 };
 
