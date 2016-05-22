6f7d8f820d4d9ab8db937886bc2b3de5e071d382
janezz55 <janezz55@gmail.com>
2016-05-22 20:29:02 +0200

    some fixes

--- contains.hpp
+++ contains.hpp
@@ -2,13 +2,35 @@
 # define GENERIC_CONTAINS_HPP
 # pragma once
 
+#include <type_traits>
+
 namespace generic
 {
 
 // contains
 //////////////////////////////////////////////////////////////////////////////
 template <class Container>
-inline bool contains(Container const& c,
+inline ::std::enable_if_t<
+  !::std::is_member_function_pointer<&Container::find>,
+  bool
+>
+contains(Container const& c,
+  typename Container::key_type const& key) noexcept(
+    noexcept(::std::find(c.begin(), c.end, key))
+)
+{
+  auto const end(c.end());
+
+  return end != ::std::find(c.begin(), end, key);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+template <class Container>
+inline ::std::enable_if_t<
+  ::std::is_member_function_pointer<&Container::find>,
+  bool
+>
+contains(Container const& c,
   typename Container::key_type const& key) noexcept
 {
   return c.end() != c.find(key);
