271eee381b52f8b282b757e3d293666a01e6c708
janezz55 <janezz55@gmail.com>
2016-08-19 13:18:52 +0200

    some fixes

--- url.hpp
+++ url.hpp
@@ -15,9 +15,8 @@ class url
 {
   ::std::string url_;
 
-  ::std::pair<::std::string::size_type, ::std::string::size_type> protocol_;
-  ::std::pair<::std::string::size_type, ::std::string::size_type> domain_;
-  ::std::pair<::std::string::size_type, ::std::string::size_type> port_;
+  ::std::pair<::std::string::size_type, ::std::string::size_type> scheme_;
+  ::std::pair<::std::string::size_type, ::std::string::size_type> authority_;
   ::std::pair<::std::string::size_type, ::std::string::size_type> path_;
   ::std::pair<::std::string::size_type, ::std::string::size_type> query_;
   ::std::pair<::std::string::size_type, ::std::string::size_type> fragment_;
@@ -32,7 +31,7 @@ public:
   }
 
   template <typename A>
-  auto& operator=(A&& u)
+  auto& operator=(A&& a)
   {
     assign(::std::forward<A>(a));
 
@@ -40,6 +39,12 @@ public:
   }
 
   operator ::std::string const& () const noexcept;
+
+  auto scheme() const;
+  auto authority() const;
+  auto path() const;
+  auto query() const;
+  auto fragment() const;
 
   void assign(::std::string);
 
@@ -49,16 +54,58 @@ public:
 };
 
 //////////////////////////////////////////////////////////////////////////////
-inline url::operator ::std::string const& () const
+inline url::operator ::std::string const& () const noexcept
 {
   return url_;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto url::scheme() const
+{
+  return is_valid() ?
+    ::std::string(url_, scheme_.first, scheme_.second) :
+    ::std::string();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto url::authority() const
+{
+  return is_valid() ?
+    ::std::string(url_, authority_.first, authority_.second) :
+    ::std::string();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto url::path() const
+{
+  return is_valid() ?
+    ::std::string(url_, path_.first, path_.second) :
+    ::std::string();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto url::query() const
+{
+  return is_valid() ?
+    ::std::string(url_, query_.first, query_.second) :
+    ::std::string();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+inline auto url::fragment() const
+{
+  return is_valid() ?
+    ::std::string(url_, fragment_.first, fragment_.second) :
+    ::std::string();
 }
 
 //////////////////////////////////////////////////////////////////////////////
 inline void url::assign(::std::string u)
 {
   // extract url info
-  ::std::regex ex("(.+)://([^/ :]+):?([^/ ]*)(/?[^ #?]*)\\x3f?([^ #]*)#?([^ ]*)");
+  ::std::regex ex(
+    R"(^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?)"
+  );
 
   ::std::cmatch what;
 
@@ -66,20 +113,11 @@ inline void url::assign(::std::string u)
 
   if ((valid_ = ::std::regex_match(c_str, what, ex)))
   {
-/*
-    cout << "protocol: " << string(what[1].first, what[1].second) << endl;
-    cout << "domain:   " << string(what[2].first, what[2].second) << endl;
-    cout << "port:     " << string(what[3].first, what[3].second) << endl;
-    cout << "path:     " << string(what[4].first, what[4].second) << endl;
-    cout << "query:    " << string(what[5].first, what[5].second) << endl;
-    cout << "fragment: " << string(what[6].first, what[6].second) << endl;
-*/
-    protocol_ = {what[1].first - c_str, what[1].second - what[1].first};
-    domain_ = {what[2].first - c_str, what[2].second - what[2].first};
-    port_ = {what[3].first - c_str, what[3].second - what[3].first};
-    path_ = {what[4].first - c_str, what[4].second - what[4].first};
-    query_ = {what[5].first - c_str, what[5].second - what[5].first};
-    fragment_ = {what[6].first - c_str, what[6].second - what[6].first};
+    scheme_ = {what[2].first - c_str, what[2].second - what[2].first};
+    authority_ = {what[4].first - c_str, what[4].second - what[4].first};
+    path_ = {what[5].first - c_str, what[5].second - what[5].first};
+    query_ = {what[7].first - c_str, what[7].second - what[7].first};
+    fragment_ = {what[9].first - c_str, what[9].second - what[9].first};
 
     url_ = ::std::move(u);
   }
@@ -97,7 +135,7 @@ inline bool url::is_valid() const noexcept
 }
 
 //////////////////////////////////////////////////////////////////////////////
-inline ::std::string const& to_string() const noexcept
+inline ::std::string const& url::to_string() const noexcept
 {
   return *this;
 }
