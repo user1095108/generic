11b6285de60eee15f28d3d96917496ab98469a0f
Janez Zemva <janezz55@gmail.com>
2017-08-09 17:58:12 +0200

    some fixes

---
+++ find.hpp
@@ -0,0 +1,49 @@
+#ifndef GNR_FIND_HPP
+# define GNR_FIND_HPP
+# pragma once
+
+#include <algorithm>
+
+#include <type_traits>
+
+namespace gnr
+{
+
+namespace
+{
+
+template <typename C, typename = std::size_t>
+struct has_find: std::false_type
+{
+};
+
+template <typename C>
+struct has_find<C,
+  decltype(
+    sizeof(
+      (typename C::iterator(C::*)(typename C::key_type const&))(&C::find)
+    )
+  )
+> : std::true_type
+{
+};
+
+}
+
+template <class Container, class Key, typename F>
+inline std::enable_if_t<!has_find<Container>{}>
+find(Container& c, Key const& k, F&& f) noexcept
+{
+  f(std::find(c.begin(), c.end(), k));
+}
+
+template <class Container, class Key, typename F>
+inline std::enable_if_t<has_find<Container>{}>
+find(Container& c, Key const& k, F&& f) noexcept
+{
+  f(c.find(k));
+}
+
+}
+
+#endif // GNR_FIND_HPP
