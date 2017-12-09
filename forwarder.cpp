43ab062aa6911bb29b75b05008c78366fb437ee2
Janez Zemva <janezz55@gmail.com>
2017-12-09 22:20:44 +0100

    some fixes

---
+++ forwarder.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "forwarder.hpp"
+
+struct S
+{
+  void f() const
+  {
+    std::cout << "Hello world!" << std::endl;
+  }
+};
+
+int main()
+{
+  S const s;
+
+  gnr::forwarder<void(S const&)> f(&S::f);
+
+  f(s);
+
+  gnr::forwarder<void(S const*)> g(&S::f);
+
+  g(&s);
+
+  gnr::forwarder<void()> h([&]() { s.f(); });
+
+  h();
+
+  return 0;
+}
