0217bf89095220aa68d8991df74d507de75a1435
Janez Zemva <janezz55@gmail.com>
2018-01-02 13:40:11 +0100

    some fixes

--- alloca.cpp
+++ alloca.cpp
@@ -6,17 +6,17 @@
 
 int main()
 {
-  std::vector<char> c{'H', 'e', 'l', 'l', 'o'};
-
-  gnr::c_str(c,
+  auto const l(
     [](char const* const p) noexcept{ std::cout << p << std::endl; }
   );
+
+  std::vector<char> c{'H', 'e', 'l', 'l', 'o'};
+
+  gnr::c_str(c, l);
 
   std::string_view s("Hello");
 
-  gnr::c_str(s,
-    [](char const* const p) noexcept{ std::cout << p << std::endl; }
-  );
+  gnr::c_str(s, l);
 
   return 0;
 }
