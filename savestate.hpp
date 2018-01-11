cbe86ecab919bacf3ea6ff4546cf07dbe3d7635f
Janez Zemva <janezz55@gmail.com>
2018-01-11 16:29:46 +0100

    some fixes

--- savestate.hpp
+++ savestate.hpp
@@ -100,6 +100,7 @@ __forceinline bool savestate(statebuf& ssb) noexcept
 #define restorestate(SSB)          \
   asm volatile (                   \
     "ldr sp, %0\n\t"               \
+    "ldr pc, %1"                   \
     :                              \
     : "m" (SSB.sp), "m" (SSB.label)\
   );
