a65961fbd76cd4dbdf02876e793172d5e9669973
Janez Zemva <janezz55@gmail.com>
2018-01-11 14:48:12 +0100

    some fixes

--- savestate.hpp
+++ savestate.hpp
@@ -39,7 +39,7 @@ inline bool __attribute__((always_inline)) savestate(statebuf& ssb) noexcept
 	);
 #elif defined(__arm__)
   asm volatile (
-    "mov %0, %%sp\nt" // store sp
+    "mov %0, %%sp\n\t" // store sp
     "mov %1, $1f\n\t" // store label
 		"mov %2, $0\n\t" // return false
     "b 2f\n\t"
