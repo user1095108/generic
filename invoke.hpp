28a45cb5da3b8baed901ce158d528235d370b239
Janez Žemva <janezz55@gmail.com>
2021-09-03 18:12:34 +0200

    some fixes

--- invoke.hpp
+++ invoke.hpp
@@ -13,17 +13,17 @@ namespace detail::invoke
 constexpr bool is_nothrow_invocable(auto&& f, auto&& t) noexcept
 {
   return [&]<auto ...I>(std::index_sequence<I...>) noexcept
-    {
-      return noexcept(
-        std::invoke(
-          std::forward<decltype(f)>(f),
-          std::get<I>(std::forward<decltype(t)>(t))...
-        )
-      );
-    }(std::make_index_sequence<
-        std::tuple_size_v<std::remove_reference_t<decltype(t)>>
-      >()
+  {
+    return noexcept(
+      std::invoke(
+        std::forward<decltype(f)>(f),
+        std::get<I>(std::forward<decltype(t)>(t))...
+      )
     );
+  }(std::make_index_sequence<
+      std::tuple_size_v<std::remove_reference_t<decltype(t)>>
+    >()
+  );
 }
 
 template <std::size_t N>
@@ -32,15 +32,15 @@ constexpr auto split(auto&& t) noexcept requires(bool(N))
   constexpr auto n(std::tuple_size_v<std::remove_cvref_t<decltype(t)>>);
   static_assert(n && !(n % N));
   return [&]<auto ...I>(std::index_sequence<I...>) noexcept
-    {
-      return std::make_tuple(
-        [&]<auto ...J>(std::index_sequence<J...>) noexcept
-        {
-          constexpr auto K(N * I);
-          return std::forward_as_tuple(std::get<K + J>(t)...);
-        }(std::make_index_sequence<N + I - I>())...
-      );
-    }(std::make_index_sequence<n / N>());
+  {
+    return std::make_tuple(
+      [&]<auto ...J>(std::index_sequence<J...>) noexcept
+      {
+        constexpr auto K(N * I);
+        return std::forward_as_tuple(std::get<K + J>(t)...);
+      }(std::make_index_sequence<N + I - I>())...
+    );
+  }(std::make_index_sequence<n / N>());
 }
 
 }
@@ -55,22 +55,22 @@ constexpr decltype(auto) apply(auto&& f, auto&& t) noexcept(
 )
 {
   return [&]<auto ...I>(std::index_sequence<I...>) noexcept(
-      noexcept(
-        std::invoke(
-          std::forward<decltype(f)>(f),
-          std::get<I>(std::forward<decltype(t)>(t))...
-        )
-      )
-    )
-    {
-      return std::invoke(
+    noexcept(
+      std::invoke(
         std::forward<decltype(f)>(f),
         std::get<I>(std::forward<decltype(t)>(t))...
-      );
-    }(std::make_index_sequence<
-        std::tuple_size_v<std::remove_reference_t<decltype(t)>>
-      >()
+      )
+    )
+  )
+  {
+    return std::invoke(
+      std::forward<decltype(f)>(f),
+      std::get<I>(std::forward<decltype(t)>(t))...
     );
+  }(std::make_index_sequence<
+      std::tuple_size_v<std::remove_reference_t<decltype(t)>>
+    >()
+  );
 }
 
 constexpr auto invoke_all(auto f, auto&& ...a) noexcept(noexcept(
