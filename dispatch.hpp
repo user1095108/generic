3496c2b146a765893fa7f4747e3c79c6ae1f8c06
Janez Žemva <janezz55@gmail.com>
2021-11-22 21:14:27 +0100

    some fixes

--- dispatch.hpp
+++ dispatch.hpp
@@ -9,21 +9,29 @@
 namespace gnr
 {
 
-constexpr auto dispatch(auto const i, auto&& ...f)
+constexpr decltype(auto) dispatch(auto const i, auto&& ...f)
   noexcept(noexcept((f(), ...)))
   requires(std::is_enum_v<std::remove_const_t<decltype(i)>>)
 {
-  using int_t = std::underlying_type_t<std::remove_cvref_t<decltype(i)>>;
+  using int_t = std::underlying_type_t<std::remove_const_t<decltype(i)>>;
 
   using tuple_t = std::tuple<decltype(f)...>;
   using R = decltype(std::declval<std::tuple_element_t<0, tuple_t>>()());
 
   return [&]<auto ...I>(std::integer_sequence<int_t, I...>)
-    noexcept(noexcept((f(), ...)))
+    noexcept(noexcept((f(), ...))) -> decltype(auto)
   {
     if constexpr(std::is_void_v<R>)
     {
       ((I == int_t(i) ? (f(), 0) : 0), ...);
+    }
+    else if constexpr(std::is_reference_v<R>)
+    {
+      std::remove_reference_t<R>* r{};
+
+      ((I == int_t(i) ? (r = &f(), 0) : 0), ...);
+
+      return *r;
     }
     else
     {
