06cfbb0023a8717f994fb453d1d577236cbea186
Janez Žemva <janezz55@gmail.com>
2022-02-11 22:17:47 +0100

    some fixes

--- invoke.cpp
+++ invoke.cpp
@@ -22,5 +22,21 @@ int main()
 
   std::cout << noexcept(gnr::chain_apply(0, inc, inc, inc, inc)) << std::endl;
 
+  //
+  auto const fib([](auto const a, decltype(a) b) noexcept
+    {
+      return std::tuple(b, a + b);
+    }
+  );
+
+  gnr::chain_apply(
+    std::tuple(0, 1),
+    fib,
+    fib,
+    fib,
+    fib,
+    [](auto, auto const b) { std::cout << b << std::endl; }
+  );
+
   return 0;
 }
