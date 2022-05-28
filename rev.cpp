8a9fe28425d8296e9113ef1eeb7c88c3dd0fe97b
Janez Žemva <janezz55@gmail.com>
2022-05-28 16:08:43 +0200

    some fixes

--- rev.cpp
+++ rev.cpp
@@ -6,30 +6,28 @@
 
 int main(int, char*[])
 {
-  for (auto& i: gnr::rev({1, 2, 3, 4}))
+  for (auto& i: gnr::rev{{1, 2, 3, 4}})
   {
     std::cout << i << std::endl;
-    i = 0;
   }
 
   int const a[]{1, 2, 3, 4};
 
-  for (auto i: gnr::rev(a))
+  for (auto i: gnr::rev{a})
   {
     std::cout << i << std::endl;
   }
 
   std::vector<int> r{1, 2, 3, 4};
 
-  for (auto& i: gnr::rev(r))
+  for (auto& i: gnr::rev{r})
   {
     std::cout << i << std::endl;
     i = 0;
   }
 
-  for (auto& i: gnr::rev(std::vector<int>{1, 2, 3, 4}))
+  for (auto& i: gnr::rev{std::vector<int>{1, 2, 3, 4}})
   {
     std::cout << i << std::endl;
-    i = 0;
   }
 }
--- rev.hpp
+++ rev.hpp
@@ -2,60 +2,29 @@
 # define GNR_REV_HPP
 # pragma once
 
+#include <initializer_list>
 #include <iterator>
-
 #include <utility>
 
 namespace gnr
 {
 
-namespace detail
-{
-
 template <typename T>
-class rev_impl
+struct rev
 {
-  T ref_;
+  static_assert(std::is_reference_v<T>);
+  T r_;
 
-public:
-  template <typename U>
-  explicit rev_impl(U&& r) noexcept(noexcept(T(std::forward<U>(r)))) :
-    ref_(std::forward<U>(r))
-  {
-  }
-
-  template <typename U, std::size_t N>
-  explicit rev_impl(U(&&a)[N]) noexcept(
-    noexcept(std::move(std::begin(a), std::end(a), std::begin(ref_))))
-  {
-    std::move(std::begin(a), std::end(a), std::begin(ref_));
-  }
-
-  auto begin() noexcept(noexcept(std::rbegin(ref_)))
-  {
-    return std::rbegin(ref_);
-  }
-
-  auto end() noexcept(noexcept(std::rend(ref_)))
-  {
-    return std::rend(ref_);
-  }
+  //
+  auto begin() noexcept(noexcept(std::rbegin(r_))) { return std::rbegin(r_); }
+  auto end() noexcept(noexcept(std::rend(r_))) { return std::rend(r_); }
 };
 
-}
+template <typename T>
+rev(std::initializer_list<T>) -> rev<std::initializer_list<T>&&>;
 
 template <typename T>
-auto rev(T&& r) noexcept(noexcept(detail::rev_impl<T>(std::forward<T>(r)))) ->
-  decltype(std::begin(r),std::end(r),detail::rev_impl<T>(std::forward<T>(r)))
-{
-  return detail::rev_impl<T>(std::forward<T>(r));
-}
-
-template <typename T, std::size_t N>
-auto rev(T(&&a)[N]) noexcept(noexcept(detail::rev_impl<T[N]>(std::forward<T[N]>(a))))
-{
-  return detail::rev_impl<T[N]>(std::forward<T[N]>(a));
-}
+rev(T&&) -> rev<T&&>;
 
 }
 
